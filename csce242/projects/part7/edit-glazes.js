1ce068825e205f024e3f40799112af01fd67697d
Dylan-B-Johnson <dmaxbear123@gmail.com>
2024-10-27 23:54:43 -0400

    split glazes  into multiple functions

--- csce242/projects/part7/edit-glazes.js
+++ csce242/projects/part7/edit-glazes.js
@@ -6,30 +6,23 @@ const getRecipes = async() => {
     }
 };
 
-const getRecipeDiv = (recipe) => {
-    const placcard = document.createElement("div");
-    placcard.classList.add("placcard");
-    placcard.classList.add("placcard-glazes");
-    const title = document.createElement("p");
-    title.innerHTML = recipe.name;
+const getDeleteDiv = (recipe) => {
+    const deleteDiv = document.createElement("div");
+    const deleteBtn = document.createElement("a");
+    const deleteH = document.createElement("h3");
+    deleteDiv.classList.add("delete-button");
+    deleteBtn.setAttribute("href", "#");
+    deleteH.innerHTML = "Delete";
+    deleteBtn.append(deleteH);
+    deleteDiv.append(deleteBtn);
+    return deleteDiv;
+};
+
+const getTableDiv = (recipe) => {
     const table = document.createElement("table");
-    const img = document.createElement("img");
-    img.setAttribute("src", recipe.image);
-    img.classList.add("one");
-    img.classList.add("contain");
-    const imgDiv = document.createElement("div");
-    imgDiv.classList.add("one", "img-div");
-    imgDiv.append(img);
     const tableDiv = document.createElement("div");
     tableDiv.classList.add("one");
     tableDiv.classList.add("table-div");
-    const mobileDiv = document.createElement("div");
-    mobileDiv.classList.add("mobile-div");
-    mobileDiv.classList.add("columns-mobile");
-    //placcard.append(title);
-    placcard.append(mobileDiv);
-    mobileDiv.append(imgDiv);
-    mobileDiv.append(tableDiv);
     const cone = document.createElement("p");
     cone.classList.add("cone");
     cone.innerHTML = "Cone: " + recipe.cone;
@@ -58,15 +51,30 @@ const getRecipeDiv = (recipe) => {
     a.setAttribute("href", recipe.link);
     a.innerHTML = recipe.credit;
     //tableDiv.append(a);
-    const deleteDiv = document.createElement("div");
-    const deleteBtn = document.createElement("a");
-    const deleteH = document.createElement("h3");
-    deleteDiv.classList.add("delete-button");
-    deleteBtn.setAttribute("href", "#");
-    deleteH.innerHTML = "Delete";
-    deleteBtn.append(deleteH);
-    deleteDiv.append(deleteBtn);
-    tableDiv.append(deleteDiv);
+    tableDiv.append(getDeleteDiv(recipe));
+    return tableDiv;
+};
+
+const getRecipeDiv = (recipe) => {
+    const placcard = document.createElement("div");
+    placcard.classList.add("placcard");
+    placcard.classList.add("placcard-glazes");
+    const title = document.createElement("p");
+    title.innerHTML = recipe.name;
+    const img = document.createElement("img");
+    img.setAttribute("src", recipe.image);
+    img.classList.add("one");
+    img.classList.add("contain");
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("one", "img-div");
+    imgDiv.append(img);
+    const mobileDiv = document.createElement("div");
+    mobileDiv.classList.add("mobile-div");
+    mobileDiv.classList.add("columns-mobile");
+    //placcard.append(title);
+    placcard.append(mobileDiv);
+    mobileDiv.append(imgDiv);
+    mobileDiv.append(getTableDiv(recipe));
     return placcard;
 };
 
